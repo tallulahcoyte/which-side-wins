ffceea154468279b224e760ce643a9c9e6d1bb7e
tallulahcoyte <72612852+tallulahcoyte@users.noreply.github.com>
2025-10-22 20:08:46 +0100

    file structure

---
+++ data-collecting/data-scraping.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+# barnet business data
+empty_properties_data = pd.read_csv("/Users/tallulahcoyte/github/which-side-wins/data/2025-10-08 Empty Properties.csv")
+business_rates_data = pd.read_csv("/Users/tallulahcoyte/github/which-side-wins/data/2025-10-08 Business Rates Register.csv")
